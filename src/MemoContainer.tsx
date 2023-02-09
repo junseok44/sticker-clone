0d7f43e5bbd21cfcb999b61b6341acb4bbf16c5d
junseok <newtonjjang@naver.com>
2023-02-09 14:49:39 +0900

    refactoring

---
+++ src/MemoContainer.tsx
@@ -0,0 +1,119 @@
+import { toJS } from "mobx";
+import React, { useState, useCallback } from "react";
+import Memo from "./Memo";
+import { TmovingObj, TtodoStore } from "./types";
+import { observer } from "mobx-react";
+
+const MemoContainer = ({ store }: { store: TtodoStore }) => {
+  const [movingObj, setmovingObj] = useState<TmovingObj | null>(null);
+  const [currentMemoId, setcurrentMemoId] = useState<number | null>(null);
+
+  const addMemo = useCallback(() => {
+    store.addMemo();
+  }, [store]);
+
+  const changePos = useCallback(
+    (id: number, xPos: number, yPos: number) => {
+      store.changePosition(id, xPos, yPos);
+    },
+    [store]
+  );
+
+  const changeZIndex = useCallback(
+    (id: number) => {
+      store.changeZIndex(id);
+    },
+    [store]
+  );
+
+  const editMemo = useCallback(
+    (id: number, msg: string) => {
+      store.editMemo(id, msg);
+    },
+    [store]
+  );
+
+  const onMouseMove = useCallback(
+    (e: React.MouseEvent) => {
+      if (movingObj !== null) {
+        // console.log("moving", e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        // FIXME e.nativeEvent.offsetX e.nativeEvent.offsetY가 계속 바뀐다.
+        // e를 보내는게 컨테이너였다가 헤더였다가.. 그러니까 e의 인식이.
+        e.stopPropagation();
+
+        changePos(
+          movingObj.id,
+          e.nativeEvent.pageX - 290 - movingObj.offsetX,
+          e.nativeEvent.pageY - movingObj.offsetY
+        );
+
+        // FIXME 일단은 200만큼만 뺏다. 그런데 다른 방법이 없나?
+
+        store.changeZIndex(movingObj.id);
+      }
+    },
+    [movingObj, changePos]
+  );
+
+  const changeSize = useCallback(
+    (id: number, width: number | undefined, height: number | undefined) => {
+      if (width && height) {
+        store.changeSize(id, width, height);
+      }
+    },
+    [store]
+  );
+
+  const deleteMemo = useCallback((id: number) => {
+    store.deleteMemo(id);
+  }, []);
+  return (
+    <div
+      style={{
+        position: "relative",
+        border: "1px solid black",
+        width: "75%",
+        height: "100vh",
+      }}
+      onClick={() => {
+        // 모든 focus 해제하기.
+        setcurrentMemoId(null);
+      }}
+      onMouseMove={(e: React.MouseEvent) => {
+        onMouseMove(e);
+      }}
+    >
+      <button onClick={addMemo}>
+        addMemo {movingObj?.id} {currentMemoId}
+      </button>
+      <button
+        onClick={() => {
+          console.log(
+            store.todo.map((item) => {
+              return toJS(item);
+            })
+          );
+        }}
+      >
+        console todo List
+      </button>
+      {store.todo.map((todo, index) => (
+        <Memo
+          currentMemoId={currentMemoId}
+          setcurrentMemoId={setcurrentMemoId}
+          key={index}
+          item={todo}
+          setmovingObj={setmovingObj}
+          editMemo={editMemo}
+          deleteMemo={deleteMemo}
+          addMemo={addMemo}
+          changeZIndex={changeZIndex}
+          changeSize={changeSize}
+          onMouseMove={onMouseMove}
+        ></Memo>
+      ))}
+    </div>
+  );
+};
+
+export default observer(MemoContainer);
---
+++ src/MemoDetailPage.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useParams, Link, useOutletContext } from "react-router-dom";
+
+const MemoDetailPage = () => {
+  const params = useParams();
+
+  const store1 = useOutletContext();
+
+  useEffect(() => {
+    console.log(store1);
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "25%",
+        height: "100vh",
+        padding: "1rem",
+        boxSizing: "border-box",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+      }}
+    >
+      <div>
+        {params.category ? "#" + params.category : "loading..."}
+        <button>메모 작성</button>
+        <Link to={"/"}>돌아가기</Link>
+      </div>
+      <input placeholder={`${params.category}에서 찾아보기..`}></input>
+      <div style={{ width: "100%", border: "1px solid black", height: "100%" }}>
+        메모들
+      </div>
+    </div>
+  );
+};
+
+export default MemoDetailPage;
