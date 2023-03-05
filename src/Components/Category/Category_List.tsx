9c08c10691553ee7cd018ea36ffc70e63943a197
junseok <newtonjjang@naver.com>
2023-03-05 16:15:16 +0900

    category & memolist css

--- src/Components/Category/Category_List.tsx
+++ src/Components/Category/Category_List.tsx
@@ -3,6 +3,8 @@ import { TdeleteCategory, TtodoStore } from "../../lib/types";
 import CategoryListItem from "./Category_List_Item";
 import { observer } from "mobx-react";
 import { Grid } from "@mui/material";
+import ListSubheader from "@mui/material/ListSubheader";
+import Divider from "@mui/material/Divider";
 
 interface MemoCategoryListProps {
   store: TtodoStore;
@@ -11,9 +13,12 @@ interface MemoCategoryListProps {
 
 const MemoCategoryList = ({ store, deleteCategory }: MemoCategoryListProps) => {
   return (
-    <div>
-      카테고리<hr></hr>
-      <Grid container spacing={1}>
+    <div style={{ maxHeight: "6rem", overflow: "auto" }}>
+      <ListSubheader component="div" id="nested-list-subheader">
+        카테고리
+        <Divider></Divider>
+      </ListSubheader>
+      <Grid container>
         {store.category.map((cat) => (
           <Grid item md={12} lg={6}>
             <CategoryListItem
@@ -24,24 +29,6 @@ const MemoCategoryList = ({ store, deleteCategory }: MemoCategoryListProps) => {
           </Grid>
         ))}
       </Grid>
-      {/* <div
-        style={{
-          width: "100%",
-          minHeight: "3rem",
-          // border: "1px solid black",
-          display: "grid",
-          gridTemplateColumns: "repeat(2, 2fr)",
-          gridAutoRows: "2rem",
-        }}
-      >
-        {store.category.map((cat) => (
-          <CategoryListItem
-            key={cat.id}
-            item={cat}
-            deleteCategory={deleteCategory}
-          ></CategoryListItem>
-        ))}
-      </div> */}
     </div>
   );
 };
